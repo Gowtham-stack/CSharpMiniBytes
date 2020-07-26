c23e314a07c8ffc624bd1f332c86c16f1a93f3ac
Gowtham-stack <66103918+Gowtham-stack@users.noreply.github.com>
2020-07-26 02:37:26 -0400

    Initial Commit
    Initial Commit

---
+++ DelegatesExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DelegatesExample
+{
+    public delegate void Mydelegate(String name);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mydelegate del3 = delegate (string name)
+            { Console.WriteLine($"Notification received for: {name}"); };
+
+            del3("mmm");
+        }
+        static void kilo(int a, int b)
+        {
+            //do something
+            Console.WriteLine("My program working fine");
+
+
+        }
+    
+    }
+}
